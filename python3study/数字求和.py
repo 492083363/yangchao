45b87932dd6308ab446eea13f289e26144ea9efc
yangchao <33111224+492083363@users.noreply.github.com>
2019-03-04 22:44:09 +0800

    Update 数字求和.py

--- python3study/数字求和.py
+++ python3study/数字求和.py
@@ -10,3 +10,6 @@ sum = float(num1)+float(num2)
 
 #显示计算结果
 print ('数字{0} 和 {1} 相加结果为： {2}'.format(num1, num2 ,sum))
+
+#另一种写法
+print('两数之和为 %.1f' %(float(input('输入第一个数字：'))+float(input('输入第二个数字：'))))
