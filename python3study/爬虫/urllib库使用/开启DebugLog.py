345463c42f48b892bc61a30aacf95e7036738891
yangchao <33111224+492083363@users.noreply.github.com>
2019-03-14 15:18:34 +0800

    Create 开启DebugLog.py

---
+++ python3study/爬虫/urllib库使用/开启DebugLog.py
@@ -0,0 +1,10 @@
+#！/bin/env
+#coding=utf-8
+#开启DebugLog
+
+import urllib.request
+httphd=urllib.request.HTTPHandler(debuglevel=1)
+httpshd=urllib.request.HTTPHandler(debuglevel=1)
+opener=urllib.request.build_opener(httphd,httpshd)
+urllib.request.install_opener(opener)
+data=urllib.request.urlopen("http://edu.51cto.com")
