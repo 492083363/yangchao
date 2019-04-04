cc7b85f83229a8edae5dd844b75ee379cb05221d
yangchao <yangchao@localhost.localdomain>
2019-04-04 23:25:08 +0800

    en1

---
+++ python3study/爬虫/爬虫伪装/爬取百度.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+import urllib.request
+import http.cookiejar
+url = "http://www.baidu.com"
+# 以字典的形式设置headers
+headers = ("Accept": " text/html, application/xhtml+xml, application/xml; q=0.9, */*;q=0.8",
+                                        "Accept-Encoding":" gb2312,utf-8",
+                                        "Accept-Language": " zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+                                        "User-Agent": "Mozilia/5.0 (Windows NT 6.1; WOW64) AppleWeb
+                                        Kit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 Safari /
+                                        537.36 SE 2.x MetaSr 1.0",
+                                        "Connection": "keep-alive",
+                                        "referer": "baidu.com")
+# 设置cookie
+cjar = http.cookiejar.CookieJar()
+#proxy = urllib.request.ProxyHandler(('http': "127.0.0.1:8888"))
+opener = urllib.request.build_opener(urllib.request.HTTPHandler, urllib, request.HTTPCookieProcessor(cjar)
+# 建立空间列数，为了以指定格式存储头信息
+headall=[]
+# 通过for循环遍里字典，构造出指定格式的headersx信息
+for key, vlaue in headers.items():
+    item=(key, value)
+    headall.append(item)
+# 将指定格式的headers信息添加好
+opener.request=headall
+# 将opener安装为全局
+urllib.request.install_opener(opener)
+data=urllib.request.urlopen(url).read()
+fhandle=open("/home/yangchao/桌面/桌面/pacong/part9/3.html", "wb")
+fhandle.wirte(data)
+fhandle.close()
