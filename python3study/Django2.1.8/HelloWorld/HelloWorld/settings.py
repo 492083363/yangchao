6818fb19b33455ae9579b7e062237dcaeb9baefc
492083363@qq.com <492083363@qq.com>
2019-07-08 15:39:50 +0800

    111

--- python3study/Django2.1.8/HelloWorld/HelloWorld/settings.py
+++ python3study/Django2.1.8/HelloWorld/HelloWorld/settings.py
@@ -1,3 +1,4 @@
+#--*--coding:UTF-8 --*--
 """
 Django settings for HelloWorld project.
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'TestModel',                     #add to here!
 ]
 
 MIDDLEWARE = [
--- python3study/Django2.1.8/HelloWorld/HelloWorld/urls.py
+++ python3study/Django2.1.8/HelloWorld/HelloWorld/urls.py
@@ -1,7 +1,8 @@
 #!/bin/python
 #coding=utf-8
-from django.urls import path
-from . import view
+from django.urls import *
+from . import view,django_db
 urlpatterns=[
     path('hello/',view.hello),
+    path('django_db/',django_db.django_db),
 ]
