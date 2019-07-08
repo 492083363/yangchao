91686ff41ae2f08ad97ad6cd2d48eca9d07e790c
492083363@qq.com <492083363@qq.com>
2019-07-08 17:20:22 +0800

    laoketeng

--- python3study/Django2.1.8/HelloWorld/HelloWorld/urls.py
+++ python3study/Django2.1.8/HelloWorld/HelloWorld/urls.py
@@ -1,11 +1,14 @@
 #!/bin/python
 #coding=utf-8
 from django.urls import *
+from django.urls import path
 from . import view,django_db,search,search2
+from django.contrib import admin 
 urlpatterns=[
     path('hello/',view.hello),
     path('django_db/',django_db.django_db),
     path('search_form/',search.search_form),
     path('search/',search.search),
-    path('search-post/',search2.search_post)
+    path('search-post/',search2.search_post),
+    path('admin/',admin.site.urls),
 ]
--- python3study/Django2.1.8/HelloWorld/TestModel/models.py
+++ python3study/Django2.1.8/HelloWorld/TestModel/models.py
@@ -6,3 +6,17 @@ class Test(models.Model):
 
 
 # Create your models here.
+
+class Contact(models.Model):
+    name   = models.CharField(max_length=200)
+    age    = models.IntegerField(default=0)
+    email  = models.EmailField()
+    def __unicode__(self):
+        return self.name
+ 
+class Tag(models.Model):
+    contact = models.ForeignKey(Contact,on_delete=models.CASCADE)
+    name    = models.CharField(max_length=50)
+    def __unicode__(self):
+        return self.name
+
