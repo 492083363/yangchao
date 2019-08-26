0ca5c0c68a143264ae3b3a2912dea3b72ac5b150
492083363@qq.com <492083363@qq.com>
2019-08-26 16:01:30 +0800

    en

--- python3study/Flask/flask_html1.py
+++ python3study/Flask/flask_html1.py
@@ -13,6 +13,17 @@ def  index():
     #return 'Hello World!'
     return render_template('index.html')
 
+#使用同一个视图函数，来显示不同用户的订单信息
+#<>来定义路由的参数 <>内需要起个名字
+@app.route('/orders/<int:order_id>')
+def get_order_id(order_id):
+#需要在视图函数的（）内填如参数名，那么后面的代码才能去使用
+    print (type(order_id))
+
+    #有时候，需要对路由做访问优化，订单ID应该是INT类型
+    return 'order_id %s' % order_id
+
+
 #4、启动程序
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
