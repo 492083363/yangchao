c75391f4ab7705db12caa2dfb7e6838c1901262e
492083363@qq.com <492083363@qq.com>
2019-09-03 14:22:48 +0800

    laoketeng

--- python3study/Django-blog-project/user/models.py
+++ python3study/Django-blog-project/user/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
 # Create your models here.
+#Django会为模型类提供一个objects对象，它是django.db.models.manager.Manager类型，用于数据库交互。
+#当定义模型类的时候没有指定管理器，则Django会为模型类一个obects的管理器
+#如果在模型类中手动指定管理器后，Django不再提供默认的object管理器
 
+#管理器是Django模型进行数据库查询操作的接口，Django应用的每个模型都至少拥有一个管理器
 
 class USER(models.Model):
     class Meta:  # 需要定义内部类
